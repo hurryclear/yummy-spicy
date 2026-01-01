ef05d73b578ecdbc70a0e305ba4dd5387b3d4ff5
hurryclear <hurryclear@mail.de>
2026-01-01 12:11:29 +0100

    update unified response wrapper

--- yummy-backend/yummy-common/src/main/java/com/yummy/result/PageResult.java
+++ yummy-backend/yummy-common/src/main/java/com/yummy/result/PageResult.java
@@ -8,18 +8,15 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * 封装分页查询结果
- * Encapsulate pagination query results
+ * pagination query results wrapper
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PageResult implements Serializable {
 
-    private long total; //总记录数
-    // Total records
+    private long total; // total records
 
-    private List records; //当前页数据集合
-    // Current page data collection
+    private List records; // current page data collection
 
 }
--- yummy-backend/yummy-common/src/main/java/com/yummy/result/Result.java
+++ yummy-backend/yummy-common/src/main/java/com/yummy/result/Result.java
@@ -5,19 +5,15 @@ import lombok.Data;
 import java.io.Serializable;
 
 /**
- * 后端统一返回结果
- * Backend unified return result
+ * Backend unified response wrapper
  * @param <T>
  */
 @Data
 public class Result<T> implements Serializable {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
-    // Code: 1 for success, 0 and other numbers for failure
-    private String msg; //错误信息
-    // Error message
-    private T data; //数据
-    // Data
+    private Integer code; //status code：1-success, 0-failure
+    private String msg; //error message
+    private T data; //data
 
     public static <T> Result<T> success() {
         Result<T> result = new Result<T>();
